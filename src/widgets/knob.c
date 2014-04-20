9fb5d5258e34b496d7945245665c476214b9afaf
William Light <wrl@illest.net>
2014-04-20 17:10:39 +0200

    knob: implement genuine/synthetic knob change events

--- src/widgets/knob.c
+++ src/widgets/knob.c
@@ -72,11 +72,12 @@ draw(struct rtb_element *elem)
  */
 
 static int
-dispatch_value_change_event(struct rtb_knob *self)
+dispatch_value_change_event(struct rtb_knob *self, int synthetic)
 {
 	struct rtb_knob_event event = {
-		.type = KNOB_VALUE_CHANGE,
-		.value =
+		.type   = KNOB_VALUE_CHANGE,
+		.source = synthetic ? RTB_EVENT_SYNTHETIC : RTB_EVENT_GENUINE,
+		.value  =
 			(self->value * (self->max - self->min)) + self->min,
 	};
 
@@ -84,7 +85,7 @@ dispatch_value_change_event(struct rtb_knob *self)
 }
 
 static void
-set_value_internal(struct rtb_knob *self, float new_value)
+set_value_internal(struct rtb_knob *self, float new_value, int synthetic)
 {
 	self->value = fmin(fmax(new_value, 0.f), 1.f);
 
@@ -95,7 +96,15 @@ set_value_internal(struct rtb_knob *self, float new_value)
 	rtb_elem_mark_dirty(RTB_ELEMENT(self));
 
 	if (self->state != RTB_STATE_UNATTACHED)
-		dispatch_value_change_event(self);
+		dispatch_value_change_event(self, synthetic);
+}
+
+static void
+set_value_internal_uncooked(struct rtb_knob *self, float new_value,
+		int synthetic)
+{
+	float range = self->max - self->min;
+	set_value_internal(self, (new_value - self->min) / range, synthetic);
 }
 
 /**
@@ -118,7 +127,7 @@ handle_drag(struct rtb_knob *self, const struct rtb_drag_event *e)
 	default: return 1;
 	}
 
-	set_value_internal(self, new_value);
+	set_value_internal(self, new_value, 0);
 	return 1;
 }
 
@@ -127,8 +136,7 @@ handle_mouse_down(struct rtb_knob *self, const struct rtb_mouse_event *e)
 {
 	switch (e->button) {
 	case RTB_MOUSE_BUTTON2:
-		rtb_knob_set_value(self, self->origin);
-		dispatch_value_change_event(self);
+		set_value_internal_uncooked(self, self->origin, 0);
 		return 1;
 
 	case RTB_MOUSE_BUTTON1:
@@ -154,12 +162,12 @@ handle_key(struct rtb_knob *self, const struct rtb_key_event *e)
 	switch (e->keysym) {
 	case RTB_KEY_UP:
 	case RTB_KEY_NUMPAD_UP:
-		rtb_knob_set_value(self, self->value + step);
+		set_value_internal(self, self->value + step, 0);
 		return 1;
 
 	case RTB_KEY_DOWN:
 	case RTB_KEY_NUMPAD_DOWN:
-		rtb_knob_set_value(self, self->value - step);
+		set_value_internal(self, self->value - step, 0);
 		return 1;
 
 	default:
@@ -200,7 +208,7 @@ attached(struct rtb_element *elem,
 	self->type = rtb_type_ref(window, self->type,
 			"net.illest.rutabaga.widgets.knob");
 
-	set_value_internal(self, self->value);
+	set_value_internal(self, self->value, 1);
 }
 
 static void
@@ -238,8 +246,7 @@ reflow(struct rtb_element *elem,
 void
 rtb_knob_set_value(struct rtb_knob *self, float new_value)
 {
-	float range = self->max - self->min;
-	set_value_internal(self, (new_value - self->min) / range);
+	set_value_internal_uncooked(self, new_value, 1);
 }
 
 int
