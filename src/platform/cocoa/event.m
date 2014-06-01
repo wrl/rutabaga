2116537c20f40829e0d8d3f4b868fe6323d38fed
William Light <wrl@illest.net>
2014-06-01 13:01:02 +0200

    cocoa: fix uv_loop_delete() assertion failure

--- src/platform/cocoa/event.m
+++ src/platform/cocoa/event.m
@@ -165,5 +165,11 @@ rtb_event_loop_fini(struct rutabaga *r)
 
 	rtb_loop = r->event_loop;
 	r->event_loop = NULL;
+
+	/* once to run the event handlers for the last time, and once more
+	 * to run the endgames for all of them. */
+	uv_run(rtb_loop, UV_RUN_NOWAIT);
+	uv_run(rtb_loop, UV_RUN_NOWAIT);
+
 	uv_loop_delete(rtb_loop);
 }
