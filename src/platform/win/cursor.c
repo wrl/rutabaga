36b8d0792307a299cbdee68882d1be58ebe6be8a
William Light <wrl@illest.net>
2018-04-15 08:01:04 +0200

    platform/win: build fix

--- src/platform/win/cursor.c
+++ src/platform/win/cursor.c
@@ -68,6 +68,7 @@ rtb__platform_set_cursor(struct rtb_window *win, struct rtb_mouse *mouse,
 {
 	switch (cursor) {
 	case RTB_MOUSE_CURSOR_DEFAULT:
+	case RTB_MOUSE_CURSOR_COPY: /* FIXME: windows doesn't have one of these */
 		SetCursor(LoadCursor(NULL, IDC_ARROW));
 		break;
 
